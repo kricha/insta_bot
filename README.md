c8a28484968095a8e3df676ab30a8e8a46d394df
Aleksej <krich.al.vl@gmail.com>
2017-06-25 23:17:50 +0300

    update bot for using insta_browser 0.4.5

--- README.md
+++ README.md
@@ -21,6 +21,7 @@ run liker
 insta_login=yourInstaLogin insta_password=yourInstaPassword env/bin/python liker.py
 --debug  - for console log
 --chrome - for using Chrome webdriver instead of PhantomJS
+--count 10 - for liking just 10 posts from feed
 ```
 
 liker will like all posts from your feed until last unliked
--- liker.py
+++ liker.py
@@ -1,23 +1,36 @@
 # -*- coding: utf-8 -*-
 import argparse
 import os
+import pprint
 from insta_browser import browser
 
+SCRIPT_PATH = os.path.dirname(__file__)
+COOKIE_PATH = '{}/var/cookie'.format(SCRIPT_PATH)
+SCREEN_SHOT_PATH = '{}/var/screenshot'.format(SCRIPT_PATH)
+LOGGER_FILE = '{}/var/log/insta_browser.txt'.format(SCRIPT_PATH)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--debug', help='enable debug mode', action="store_true")
 parser.add_argument('--chrome', help='using Chrome webdriver instead of PhantomJS', action="store_true")
+parser.add_argument('--count', help='if indicated, like posts by count', type=int, default=0)
 args = parser.parse_args()
 debug = args.debug
 chrome = args.chrome
+count = args.count
+
 
 login = os.environ.get('insta_login')
 password = os.environ.get('insta_password')
 
-br = browser.Browser(debug, chrome, 'var/cookies')
+exclude = open('{}/exclude_accounts.txt'.format(SCRIPT_PATH)).read().split(',')
+
+br = browser.Browser(debug, chrome, COOKIE_PATH, SCREEN_SHOT_PATH, LOGGER_FILE, exclude)
 try:
     br.auth(login, password)
-    br.scroll_feed_to_last_not_liked()
+    if count:
+        br.scroll_feed_by_posts_count(count)
+    else:
+        br.scroll_feed_to_last_not_liked_posts()
     br.like_found_posts()
 finally:
     print(br.get_summary())
