14c1ce74f2f897706070722c07631251de9af071
Aleksej <krich.al.vl@gmail.com>
2017-06-29 14:58:15 +0300

    update bot gor using insta_browser 0.6.6, add script for tag liking

---
+++ like_tag.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import argparse
+import os
+from insta_browser import browser
+
+SCRIPT_PATH = os.path.dirname(__file__)
+COOKIE_PATH = '{}/var/cookie'.format(SCRIPT_PATH)
+SCREEN_SHOT_PATH = '{}/var/screenshot'.format(SCRIPT_PATH)
+LOGGER_FILE = '{}/var/log/insta_browser.txt'.format(SCRIPT_PATH)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', help='enable debug mode', action="store_true")
+parser.add_argument('--chrome', help='using Chrome webdriver instead of PhantomJS', action="store_true")
+parser.add_argument('--count', help='if indicated, like posts by count', type=int, default=None)
+parser.add_argument('--tag', help='if indicated, like posts by count', type=str, required=True)
+args = parser.parse_args()
+debug = args.debug
+chrome = args.chrome
+count = args.count
+tag = args.tag
+
+
+login = os.environ.get('insta_login')
+password = os.environ.get('insta_password')
+
+br = browser.Browser(debug, chrome, COOKIE_PATH, SCREEN_SHOT_PATH, LOGGER_FILE)
+try:
+    br.auth(login, password)
+    br.process_tag(tag)
+finally:
+    print(br.get_summary())
+    br.close_all()
